ae8a8871b1327745b0f490c9cba279e71b8c062d
Hariprasath R <balaprasad24@gmail.com>
2021-09-08 21:26:47 +0530

    Added concurrent thread consumer configuration property

--- library-events-consumer/src/main/java/in/hp/kafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ library-events-consumer/src/main/java/in/hp/kafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -1,14 +1,15 @@
 package in.hp.kafka.libraryeventsconsumer.config;
 
 import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.kafka.ConcurrentKafkaListenerContainerFactoryConfigurer;
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.listener.ContainerProperties;
 
 @Configuration
 @EnableKafka
@@ -20,15 +21,21 @@ public class LibraryEventsConsumerConfig {
         this.properties = kafkaProperties;
     }
 
-//    @Bean
-//    @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
+    @Bean
+    @ConditionalOnMissingBean(name = "kafkaListenerContainerFactory")
     ConcurrentKafkaListenerContainerFactory<?, ?> kafkaListenerContainerFactory(
             ConcurrentKafkaListenerContainerFactoryConfigurer configurer,
             ObjectProvider<ConsumerFactory<Object, Object>> kafkaConsumerFactory) {
         ConcurrentKafkaListenerContainerFactory<Object, Object> factory = new ConcurrentKafkaListenerContainerFactory<>();
         configurer.configure(factory, kafkaConsumerFactory
                 .getIfAvailable(() -> new DefaultKafkaConsumerFactory<>(this.properties.buildConsumerProperties())));
-        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        // To enable manual acknowledgement mode
+        // factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
+        // To enable concurrent consumers - parameter -> thread count
+        // factory.setConcurrency(3);
+
         return factory;
     }
 }
